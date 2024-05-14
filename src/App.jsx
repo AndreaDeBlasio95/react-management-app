0eb7b599cce4f44322b8851aced62df545d2d6c2
AndreaDeBlasio55 <39781342+AndreaDeBlasio95@users.noreply.github.com>
2024-05-14 08:39:17 +0200

    NewProject file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import ProjectsSideBar from "./components/ProjectsSideBar.jsx";
+import NewProject from "./components/NewProject.jsx";
 
 function App() {
   return (
-    <main>
+    <main className="h-screen my-8 flex gap-8">
       <ProjectsSideBar />
+      <NewProject />
     </main>
   );
 }
